84ed22fe2f113d4fa5b5817e1cda327a06da36f1
yongquan qu <qyq1381@gmail.com>
2019-01-13 03:15:27 -0600

    add admin search function

--- PowerSpring/Controllers/AdminController.cs
+++ PowerSpring/Controllers/AdminController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using PowerSpring.Models;
 using PowerSpring.Helper;
+using PowerSpring.ViewModels;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PowerSpring.Controllers
@@ -17,16 +18,39 @@ namespace PowerSpring.Controllers
         // GET: /<controller>/ 
         private readonly IWebUserRepository _webUserRepository;
         private IUserManager _userManager;
-        public AdminController(IWebUserRepository webUserRepository, IUserManager userManager) {
+        public AdminController(IWebUserRepository webUserRepository, IUserManager userManager)
+        {
             _webUserRepository = webUserRepository;
             _userManager = userManager;
         }
 
         public IActionResult Index()
         {
-            return View(_webUserRepository.GetAllWebUsers());
+            AdminViewModel adminViewModel = new AdminViewModel
+            {
+                webUsers = _webUserRepository.GetAllWebUsers(),
+                searchName = null
+            };
+            return View(adminViewModel);
         }
-        public IActionResult Mute(int id) {
+        [HttpPost]
+        public IActionResult Index(AdminViewModel adminViewModel)
+        {
+            if (adminViewModel.searchName == null)
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+            adminViewModel.webUsers = Search(adminViewModel.searchName);
+            adminViewModel.searchName = null;
+            return View(adminViewModel);
+        }
+        private IEnumerable<WebUser> Search(string searchName)
+        {
+            IEnumerable<WebUser> suspects = _webUserRepository.GetAllWebUsers().Where(p=>p.UserName.Contains(searchName));
+            return (suspects);
+        }
+        public IActionResult Mute(int id)
+        {
             var User = _webUserRepository.GetWebUserById(id);
             User.Muted = !User.Muted;
             _userManager.Update(User);
